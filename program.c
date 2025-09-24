6bbf1252a6ce8ab42cc274c35397fc8ba21344a8
codeEnjoyer7 <nicolas.nl.londono@gmail.com>
2025-09-24 16:48:45 +0000

    experimenting

---
+++ program.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //input output library, lets you use printf
+#include <math.h> //math library
+#include <stdlib.h> // library that lets you manage memory
+
+
+
+void main(){
+
+    int map[9][9]={
+        {1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1},
+    };
+    int playerX=4;
+    int playerY=4;
+
+}
