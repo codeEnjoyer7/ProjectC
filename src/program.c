df1773a6fdec02ce7a5fa529825de64a2740a3b4
codeEnjoyer7 <nicolas.nl.londono@gmail.com>
2025-09-25 13:01:27 +0000

    saving progress, working on adding OpenGL

---
+++ src/program.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //input output library, lets you use printf
+#include <math.h> //math library
+#include <stdlib.h> // library that lets you manage memory
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+void main(){
+
+    printf("Hello World!");
+
+}
